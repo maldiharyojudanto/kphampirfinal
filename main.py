023f445e8abf1d680ef2ea65a514b0c8ff79f792
maldiharyojudanto <76139419+maldiharyojudanto@users.noreply.github.com>
2023-08-23 16:10:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -346,8 +346,6 @@ allFakultas = {"FIF": ['S1 Informatika', 'S1 Teknologi Informasi', 'S2 Informati
             'FIT':['D3 Sistem Informasi', 'D3 Teknologi Komputer', 'D3 Sistem Informasi Akuntansi', 'D3 Manajemen Pemasaran', 'D3 Teknologi Telekomunikasi', 'D3 Rekayasa Perangkat Lunak Aplikasi', 'D3 Perhotelan', 'D4 Teknologi Rekayasa Multimedia']}
 listFakultas = allFakultas.keys()
 
-listFak = ['fif','fri','fte','feb','fkb','fik','fit']
-
 keterangan = ['Bekerja', 'Melanjutkan Pendidikan', 'Tidak Bekerja', 'Wiraswasta', 'Survey Pengguna']
 keterangan2 = ['Bekerja', 'Wiraswasta', 'Survey Pengguna']
 keterangan3 = ['Bekerja', 'Wiraswasta']
@@ -372,7 +370,7 @@ def main():
         elif selected == "Fakultas":
             options_faculty = st.selectbox(
                 'Pilih Fakultas:',
-                listFak, key="pilihFakultass")
+                listFakultas, key="pilihFakultass")
             options_keterangan = st.selectbox(
                 'Pilih Status:',
                 keterangan2, key="pilihStatus"
@@ -380,7 +378,7 @@ def main():
         elif selected == "Program Studi":
             options_faculty = st.selectbox(
                 'Pilih Fakultas:',
-                listFak, key="pilihFakultas")
+                listFakultas, key="pilihFakultas")
             options_prodi= st.selectbox(
                 'Pilih Prodi:',
                 allFakultas[options_faculty], key="pilihProdi")
@@ -401,7 +399,7 @@ def main():
         universitas(options_keterangan) # Panggil fungsi universitas
     if selected == "Fakultas":
         st.header('Selamat datang !')
-        fakultas(logofak, options_faculty, options_keterangan) # Panggil fungsi fakultas
+        fakultas(options_faculty, options_keterangan) # Panggil fungsi fakultas
     if selected == "Program Studi":
         st.header('Selamat datang !')
         programstudi(options_faculty, options_prodi, options_keterangan) # Panggil fungsi program studi
